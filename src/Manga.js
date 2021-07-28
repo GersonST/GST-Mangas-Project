c7a9f0057723e999aea7a468ec41c0ed5e310520
GeffersonST <83526000+GeffersonST@users.noreply.github.com>
2021-07-28 15:41:04 -0300

    Finalizando

--- src/Manga.js
+++ src/Manga.js
@@ -1,9 +1,15 @@
 export const Manga = ({ manga }) => {
   const { attributes } = manga;
-
+  console.log(attributes);
+  const title = attributes.titles["en"] || attributes.titles["ja_jp"];
+  const img = attributes.posterImage?.tiny;
   return (
-    <div className="p-2 shadow rounded">
-      <h3>{attributes.titles["en"]} </h3>
+    <div className="p-2 shadow-xl rounded bg-gray-200">
+      <div className="flex flex-row space-x-4 justify-start items-start">
+        <img src={img} alt={title} className="object-scale-down" />
+        <h3 className="text-xl font-bold">{title}</h3>
+        <div>{attributes.description}</div>
+      </div>
     </div>
   );
 };
