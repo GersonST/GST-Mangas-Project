9ba5c240584c0b83a7e0185628c77dddc9c65184
Gerson Severo <gersonsevero90@gmail.com>
2021-07-29 03:39:54 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { getHotManga } from "./api";
 import { MangaGrid } from "./MangaGrid";
 export default function App() {
   const [mangas, setMangas] = useState([]);
+  const [lucky, setLucky] = useState("");
+  const [char, setChar] = useState("");
 
   const getMangas = async () => {
     try {
@@ -15,6 +18,31 @@ export default function App() {
     getMangas();
   }, []);
 
+  useEffect(() => {
+    const getChar = async () => {
+      await axios
+        .get(`https://kitsu.io/api/edge/characters/${lucky}`)
+        .then((response) => {
+          console.log(response.data.data.attributes);
+          setChar(response.data.data.attributes);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getChar();
+  }, [lucky]);
+
+  function Lucky() {
+    const min = 1;
+    const max = 10000;
+
+    const rdn = Math.floor(Math.random() * (max - min));
+
+    console.log(rdn);
+    setLucky(rdn);
+  }
+
   return (
     <div className=" sm:p-4 bg-yellow-500">
       <h1 className="text-xl sm:text-4xl text-center font-bold">
@@ -26,6 +54,17 @@ export default function App() {
           📚
         </span>
       </h1>
+      <button className="btn" onClick={() => Lucky()}>
+        Try your lucky
+      </button>
+
+      {char && (
+        <div>
+          <h1>{char.name}</h1>
+          <p>{char.description}</p>
+          <img src={char.image?.original} alt={char.name} />
+        </div>
+      )}
 
       <MangaGrid mangas={mangas} />
     </div>
