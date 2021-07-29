7bc9f7d2699790abf08c0c471423b9dfc488d2d6
Gerson Severo <gersonsevero90@gmail.com>
2021-07-29 02:14:37 -0300

    initial commit

--- src/Manga.js
+++ src/Manga.js
@@ -5,11 +5,13 @@ export const Manga = ({ manga }) => {
   const img = attributes.posterImage?.small;
 
   return (
-    <div className="p-6 shadow-xl rounded bg-gray-200">
-      <div className="flex flex-row space-x-4 justify-start items-start">
-        <img src={img} alt={title} className="object-scale-down" />
-        <h3 className="text-xl font-bold">{title}</h3>
-        <div className="text-justify">{attributes.description}</div>
+    <div className="p-6 shadow-xl rounded bg-gray-200 ">
+      <div className="flex flex-col sm:flex-row space-x-4 justify-start items-start">
+        <img src={img} alt={title} className="object-scale-down shadow-2xl" />
+        <h3 className="text-lg sm:text-xl font-bold p-4 sm:p-0">{title}</h3>
+        <div className="text-justify text-sm sm:text-xl sm:bg-gray-200 bg-gray-300 p-3">
+          {attributes.description}
+        </div>
       </div>
     </div>
   );
--- src/MangaGrid.js
+++ src/MangaGrid.js
@@ -5,8 +5,8 @@ export const MangaGrid = (props) => {
 
   return (
     <div
-      className="grid md:grid-cols-2 xl:grid-cols-2 gap-4
-    p-4"
+      className="grid  md:grid-cols-1 xl:grid-cols-2 gap-4 sm:p-4
+    "
     >
       {mangas.map((manga, idx) => {
         return <Manga manga={manga} />;
